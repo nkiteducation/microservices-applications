c0391a6bc26023a4e3acacbfa812f584bd3316fd
Koryagin Nazar <koriahin.nazar@student.karazin.ua>
2025-10-04 16:35:05 +0000

    Add core settings configuration with Pydantic for database connection

---
+++ app/core/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn
+from pathlib import Path
+
+class CoreSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=Path(".").rglob(".env"),
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+class DataBase(CoreSettings):
+    url: PostgresDsn
+
+class Settings(CoreSettings):
+    database: DataBase
+
+config = Settings()
