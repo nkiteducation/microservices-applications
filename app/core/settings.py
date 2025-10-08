7afa969659b517f2e94767f9f011b062f910122e
Koryagin Nazar <koriahin.nazar@student.karazin.ua>
2025-10-08 15:06:58 +0000

    logger settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Literal
 
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -17,8 +18,17 @@ class DataBase(CoreSettings):
     url: PostgresDsn
 
 
+class Logger(CoreSettings):
+    fmt: str = (
+        "%(asctime)s[%(levelname)8s|%(name)s:%(funcName)s:%(levelno)s]  %(message)s"
+    )
+    datefmt: str = "%Y-%m-%d %H:%M:%S"
+    level: Literal["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
+
+
 class Settings(CoreSettings):
     database: DataBase
+    logger: Logger
 
 
 config = Settings()
