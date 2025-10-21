f56e3604546478d81ad16883fad3b1438dc1a623
Koryagin Nazar <koriahin.nazar@student.karazin.ua>
2025-10-21 11:37:07 +0000

    refactor: change ContactMethod to StrEnum and add Memory enum for storage units

--- app/utils/enum.py
+++ app/utils/enum.py
@@ -1,7 +1,12 @@
 import enum
 
+class Memory(enum.IntEnum):
+    B = 1
+    KB = 1024 * B
+    MB = 1024 * KB
+    GB = 1024 * MB
 
-class ContactMethod(enum.Enum):
+class ContactMethod(enum.StrEnum):
     EMAIL = "email"
     PHONE = "phone"
     TELEGRAM = "telegram"
